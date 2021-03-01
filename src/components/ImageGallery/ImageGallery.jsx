bf420ab8a44f1d347e30860b8e30a7c300edc440
Ivan Khailo <jakom333@gmail.com>
2021-03-01 22:17:29 +0200

    remove loader as component

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ImageGalleryItem from '../ImageGalleryItem'
+import ImageGalleryItem from './ImageGalleryItem'
 import PropTypes from 'prop-types';
 import styles from './ImageGallery.module.css'
 
